52b51c7a364b1884fd404a1ca6c687d6a739c4e5
Allyza <allyzaconde>
2023-01-10 17:21:44 +0800

    Add authcontext and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useContext, useState} from 'react';
+import { AuthContext } from '../context/AuthState';
 
 function Login(){
+    const { state, login } = useContext(AuthContext);
+    const [loginDetails, setLogin] = useState({
+        username: '',
+        password: '',
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        login(loginDetails.username, loginDetails.password)
+    }
+
+    const handleOnChange = (userKey, newValue) =>
+    setLogin({ ...loginDetails, [userKey]: newValue });
+
     return(
         <>
             <p>login page</p>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        onChange={(e) => handleOnChange('username', e.target.value)}
+                        placeholder="Enter Username"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="text"
+                        onChange={(e) => handleOnChange('password', e.target.value)}
+                        placeholder="Enter Password"
+                        required
+                    />
+                </div>
+                <button>Login</button>
+            </form>
+            {state.loginError ? (
+              <div>{state.loginError}</div>
+            ):<></>}
         </>
     )
 }
---
+++ src/context/AuthState.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useSetState } from 'react-use';
+import { createContext } from "react";
+
+const initialState = {
+    username: '',
+    isLoggedIn: false,
+    isLoginPending: false,
+    loginError: null
+}
+
+export const AuthContext = createContext(initialState)
+
+export const AuthProvider = ({ children }) => {
+    const [state, setState] = useSetState(initialState);
+    
+    const setLoginSuccess = (isLoggedIn) => setState({isLoggedIn});
+    const setLoginPending = (isLoginPending) => setState({isLoginPending});
+    const setLoginError = (loginError) => setState({loginError});
+
+    function login(username, password) {
+        setLoginPending(true);
+        setLoginSuccess(false);
+        setLoginError(null);
+
+        fetchLogin( username, password, error => {
+            setLoginPending(false);
+      
+            if (!error) {
+              setLoginSuccess(true);
+            } else {
+              setLoginError(error);
+            }
+        })
+    }
+
+    const logout = () => {
+        setLoginPending(false);
+        setLoginSuccess(false);
+        setLoginError(null);
+    }
+    
+    return(
+        <AuthContext.Provider 
+            value = {{
+                state,
+                login,
+                logout
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+const fetchLogin = (username, password, callback) => 
+  setTimeout(() => {
+    const loginDetails = {username: username, password: password};
+    const request = new Request(
+        "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
+        {
+          method: 'POST',
+          body: JSON.stringify(loginDetails),
+          headers: new Headers({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }),
+        }
+      );
+      fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            return callback(null);
+          }
+            return callback(new Error('Invalid username or password'));
+          })
+        .catch((error) => {
+            console.log(error);
+        });
+  }, 1000);
